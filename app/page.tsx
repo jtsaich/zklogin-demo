cce7e1d6231921d1101c956fd4a29fd3e9cd8ab7
Jack Tsai <jtsai0606@gmail.com>
2023-12-12 18:25:59 +0800

    fixing build

--- app/page.tsx
+++ app/page.tsx
@@ -108,69 +108,49 @@ export default async function Home() {
               >
                 {address}
               </p>
-              <p
-                className="animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-sm"
-                style={{
-                  animationDelay: "0.25s",
-                  animationFillMode: "forwards",
-                }}
-              >
-                Powered by{" "}
-                <a
-                  href="https://docs.sui.io/testnet/build/zk_login"
-                  target="_blank"
-                  className="underline"
-                  rel="noopener noreferrer"
-                >
-                  Sui zkLogin
-                </a>{" "}
-                - a new era of privacy-preserving web3 authentication.
-              </p>
             </div>
           </>
         )}
 
-        {!isLoggedIn() && (
-          <div className="flex flex-col gap-4">
-            <div>
-              <input
-                type="text"
-                placeholder="Search..."
-                className="input w-full max-w-lg"
-              />
-            </div>
+        <div className="flex flex-col items-center gap-4">
+          <div>
+            <input
+              type="text"
+              placeholder="Search..."
+              className="input w-full max-w-lg"
+            />
+          </div>
 
-            {places.map(({ id, websiteUri, displayName, formattedAddress }) => (
-              <div key={id} className="card lg:card-side bg-base-100 shadow-xl">
-                <figure>
-                  <img src={websiteUri} alt={displayName.text} />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{displayName.text}</h2>
-                  <p>{formattedAddress}</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Detail</button>
-                  </div>
+          {places.map(({ id, displayName, formattedAddress }) => (
+            <div key={id} className="card lg:card-side bg-base-100 shadow-xl">
+              {/* <figure>
+                <img src={websiteUri} alt={displayName.text} />
+              </figure> */}
+              <div className="card-body">
+                <h2 className="card-title">{displayName.text}</h2>
+                <p>{formattedAddress}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Detail</button>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
 
-            <p
-              className="animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-md mt-2"
-              style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+          <p
+            className="animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-md mt-2"
+            style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+          >
+            Powered by{" "}
+            <a
+              href="https://docs.sui.io/testnet/build/zk_login"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
             >
-              Powered by{" "}
-              <a
-                href="https://docs.sui.io/testnet/build/zk_login"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                Sui zkLogin
-              </a>
-            </p>
-          </div>
-        )}
+              Sui zkLogin
+            </a>
+          </p>
+        </div>
       </div>
     </>
   );
