b6eae1cc64d37d7697d8faaf262c3b7adf747741
prodoxx <reggie.escobar94@gmail.com>
2023-12-13 11:16:19 +0800

    changed name

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -39,7 +39,7 @@ export default function NavBar({
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
           <Link href="/" className="flex items-center font-display text-2xl">
-            <p>zkPoll</p>
+            <p>SuiReviews</p>
           </Link>
           <div>
             {session ? (
